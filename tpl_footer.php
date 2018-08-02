2e0592ae9e25fb662c9d91d0806f7f212149f5b0
Dolores Portalatin <admin@doloresportalatin.info>
2018-08-02 03:58:55 -0400

    updated menus and footer

--- tpl_footer.php
+++ tpl_footer.php
@@ -1,19 +1,120 @@
     <footer class="main clearfix" role="contentinfo">
       <section class="col-1-3">
         <p>&copy; Archwomen.org</p>
-        <p><a href="mailto:webmaster@archwomen.org">Report Site Issues</a></p>
-        <p><a href="mailto:admin@archwomen.org"><img alt="email" class="icon" src="/assets/images/icons/mail.png"></a>
-        <a href="http://twitter.com/archwomen"><img alt="twitter" class="icon" src="/assets/images/icons/twitter.png"></a>
-        <a href="https://github.com/archwomen"><img alt="github" class="icon" src="/assets/images/icons/github.png"></a>
-        <a href="https://plus.google.com/104352270333357228007" rel="publisher">G+</a>
-        <p>Join us on <a href="http://freenode.net/">irc.freenode.net</a> in 
-        <a href="http://webchat.freenode.net/?channels=archlinux-women">#archlinux-women</a></p>
+        <p>
+          <a href="mailto:webmaster@archwomen.org">
+            Report Site Issues
+          </a>
+        </p>
+        <p>
+          <a href="mailto:admin@archwomen.org">
+            <svg role="img" aria-label="email us" xmlns:svg="http://www.w3.org/2000/svg"
+            xmlns="http://www.w3.org/2000/svg" width="32" height="32" enable-background="new 0 0 50 50" version="1.1" viewBox="0 0 50 50" xml:space="preserve">
+              <title>mail</title>
+              <desc>mail to link</desc>
+              <path d="m7.41 8.57c-1.12 0-2.17 0.26-3.03 0.69L25.63 22.14 44.88
+              9.24C43.98 8.87 43 8.62 42.06 8.57l-34.65 0zm40.75 3.82-21.84
+              14.64-0.04-0.02-0.32 0.46-23.95-14.51 0 25.74c0 2.2 2.42 3.99 5.4
+              3.99l35.39 0c2.79-0.14 5.4-1.79
+              5.4-3.99l0-26.14c0-0.06-0.03-0.11-0.04-0.17z" fill="#fff"/>
+            </svg>
+          </a>
+          <a href="http://twitter.com/archwomen">
+            <svg role="img"  aria-label="link to twitter" xmlns:svg="http://www.w3.org/2000/svg"
+            xmlns="http://www.w3.org/2000/svg" width="32" height="32"
+            enable-background="new 0 0 50 50" version="1.1" viewBox="0 0 50 50"
+            xml:space="preserve">
+              <title>twitter</title>
+              <desc>link to twitter</desc>
+              <path d="m48.41 10.01c-1.82 0.86-4.29 1.89-6.28 2.07 1.98-1.2
+              3.96-4.13 4.79-6.54-1.49 1.03-3.3 2.24-5.12 2.93L40.65
+              7.26c-1.82-1.89-3.47-3.27-6.28-3.27-5.45 0-9.42 4.3-9.42 9.81 0
+              0.69 0 1.2 0.17 1.55l-0.17 0c-9.42 0-15.69-2.07-20.48-8.26-0.83
+              1.55-1.65 3.1-1.65 4.99 0 3.44 2.15 6.37 4.79 8.26-1.65
+              0-3.47-0.86-4.79-1.72 0 4.99 6.61 10.67 11.07 11.53-1.65 1.72-7.27
+              0.17-7.93 0 1.32 3.96 5.29 6.54 9.42 6.54-3.3 2.75-7.27 4.13-11.89
+              4.13-0.83 0-1.49 0-2.31-0.17 4.29 2.93 10.24 4.3 15.86 4.3 17.84 0
+              26.93-14.63 26.93-28.05 0-0.52 0-1.2 0-1.72 1.65-1.2 3.3-3.1
+              4.46-5.16z" fill="#fff"/>
+            </svg>
+          </a>
+          <a href="https://github.com/archwomen">
+            <svg role="img" aria-label="link to github" xmlns:svg="http://www.w3.org/2000/svg"
+            xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0"
+            width="32" height="32" viewBox="0 0 512 512" enable-background="new
+            0 0 512 512" xml:space="preserve">
+              <title>github</title>
+              <desc>link to github</desc>
+              <path d="M424.6 77.95c3.95-12.74
+               9.22-41.03-5.26-77.24 0 0-25.55-8.11-83.34 30.57
+               -22.64-5.66-46.73-8.45-70.86-8.45 -26.55 0-53.37 3.38-78.39
+               10.24C127.12-7.62 100.79 0.72 100.79 0.72c-17.19 42.97-6.56 74.79-3.33
+               82.69 -20.2 21.8-32.53 49.62-32.53 83.74 0 25.76 2.93 48.86 10.1
+               68.94l7.53 17.06c20.13 37.53 59.82 61.77 130.44 69.38 -10.02
+               6.73-20.24 18.18-24.39 37.56 -13.66 6.53-56.89 22.48-82.83-22.14 0
+               0-14.52-26.55-42.31-28.63 0 0-26.96-0.41-1.86 16.8 0 0 18.04 8.5 30.49
+               40.44 0 0 16.24 54.49 94.59 37.01v56.28c0 0-1.65 19.91-19.91 26.54 0
+               0-10.78 7.47 0.84 11.61 0 0 50.59 4.15 50.59-37.32V399.31c0
+               0-2.01-24.34 9.95-32.8v100.81c0 0-0.83 24.06-13.27 33.18 0 0-8.3 14.92
+               9.95 10.79 0 0 34.83-4.98 36.49-45.61l0.79-102.01h8.37l0.8 102.01c1.66
+               40.64 36.49 45.62 36.49 45.62 18.24 4.14 9.95-10.78 9.95-10.78
+               -12.44-9.12-13.27-33.18-13.27-33.18V367.38c11.95 9.31 9.95 31.93 9.95
+               31.93v61.37c0 41.47 50.59 37.32 50.59 37.32 11.61-4.14 0.83-11.6
+               0.83-11.6 -18.25-6.63-19.91-26.54-19.91-26.54v-80.44c0-31.37-13.21-47.99-26.13-56.75
+               75.31-7.47 111.44-31.78 128.09-69.56l5.95-16.86c5.58-20.12 7.26-43.27
+               7.26-69.09C461.09 130.11 447.18 100.49 424.6 77.95zM264.53
+               308.64c-82.09 0-148.63-12.35-148.63-91.68 0-18.99 9.35-36.62
+               25.37-51.26 26.72-24.39 71.95-11.47 123.25-11.47 51.08-0.01
+               96.15-13.05 122.91 11.17l0.01 0.01c16.22 14.7 25.71 32.44 25.71
+               51.56C413.16 306.23 346.61 308.64 264.53 308.64zM184.18 176.99c-17.08
+               0-30.92 19.92-30.92 44.51 0 24.59 13.85 44.52 30.92 44.52s30.92-19.93
+               30.92-44.52C215.1 196.92 201.25 177.01 184.18 176.99zM350.09
+               176.99c-17.08 0-30.92 19.92-30.92 44.51 0 24.59 13.84 44.52 30.92
+               44.52 17.08 0 30.92-19.93 30.92-44.52C381.02 196.92 367.17 177.01
+               350.09 176.99zM265 268.77c4.27 0 7.73-3.46 7.73-7.73
+               0-4.27-3.46-7.73-7.73-7.73 -4.27 0-7.73 3.46-7.73 7.73C257.27 265.31
+               260.73 268.77 265 268.77zM281.35 277.42c-1.3-0.46-2.72 0.22-3.17 1.51
+               -1.99 5.62-7.32 9.39-13.27 9.39 -5.94 0-11.27-3.77-13.27-9.39
+               -0.45-1.29-1.87-1.97-3.17-1.51 -1.29 0.46-1.97 1.88-1.51 3.17l0.01
+               0.02c2.69 7.61 9.89 12.69 17.94 12.69 8.04 0 15.27-5.11
+               17.95-12.7C283.31 279.29 282.64 277.88 281.35 277.42z"
+               fill="#fff"/>
+            </svg>
+          </a>
+          <a href="https://plus.google.com/104352270333357228007" rel="publisher">
+            <svg role="img" aria-label="link to google plus" xmlns:svg="http://www.w3.org/2000/svg"
+            xmlns="http://www.w3.org/2000/svg" width="32" height="32"
+            enable-background="new 0 0 50 50" version="1.1" viewBox="0 0 50 50"
+            xml:space="preserve">
+              <title>google plus</title>
+              <desc>link to google plus</desc>
+              <g
+              transform="matrix(1.3601695,0,0,1.4237288,-10.317797,-10.381356)"
+              fill="#fff">
+                <polygon points="35 23 31 23 31 25 35 25 35 29 37 29 37 25 41
+                25 41 23 37 23 37 19 35 19 " fill="#fff"/>
+                <path d="m26 27c0 0-2-1.2-2-2 0 0-0.5-1.8 1-3 1.5-1.2 3-3
+                3-5 0-2.3-1-5-3-6l3 0 2.4-1c0 0-7.1 0-9.4 0-4.2 0-8 3.3-8
+                7 0 3.7 2.8 6.6 7.1 6.6 0.3 0 0.6 0 0.9 0-0.3 0.5-0.5
+                1.1-0.5 1.7 0 1 0.8 2 1.5 2.7-0.5 0-1.4 0-2 0-5.2 0-9
+                2.6-9 6 0 3.4 4.9 6 10 6 5.9 0 10-3 10-7
+                0-2.7-2.5-4.3-5-6zm-4-4c-2.4 0-5.6-2.9-6-6-0.4-3.1 1.6-6.1
+                4-6 2.4 0.1 4.6 2.9 5 6.1 0.4 3.1-1 5.9-3 5.9zm-1 15c-3.6
+                0-7-1.3-7-4 0-2.7 3.4-5 7-5 0.8 0 2 0 2 0 1 0 2.9 0.9 4 2
+                1 1 1 2.7 1 3 0 3-2 4-7 4z" fill="#fff"/>
+              </g>
+            </svg>
+          </a>
+        <p>
+          Join us on <a href="http://freenode.net/">irc.freenode.net</a> in 
+          <a href="https://kiwiirc.com/client/irc.freenode.net/#archlinux-women">#archlinux-women</a>
+        </p>
       </section>
       <section class="col-2-3">
         <h3>Sponsors</h3>
         <p>
           <a href="https://www.digitalocean.com/">
-            <img width="140" height="39" alt="digital ocean" src="/assets/images/logos/digitalocean.png">
+            <img width="140" height="39" alt="digital ocean" src="/static/images/digitalocean.png">
           </a>
         </p>
         <p>
@@ -28,10 +129,20 @@
           <p style="margin-top:0">
             <span>Arch Women runs off <del>love</del></span>
             <input type="hidden" name="cmd" value="_s-xclick">
-            <input type="hidden" name="hosted_button_id" value="ADCGV6K9VK2MJ">
-            <input type="image" alt="coffee" src="https://archwomen.org/assets/images/icons/coffee.png" name="submit">
+            <input type="hidden" name="hosted_button_id" value="PGCY3UUYM3VUW">
+            <input type="image" alt="coffee" src=" /static/images/coffee.png" name="submit">
             <img alt="PayPal - The safer, easier way to pay online!" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
           </p>
         </form>
-      </section>
+     </section>
     </footer>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script>
+      $(document).click(function(event) {
+        if(!$(event.target).closest('nav').length) {
+          if($('#toggle').is(":checked")) {
+            $('#toggle').attr('checked', false);
+          }
+        }
+      });
+    </script>
